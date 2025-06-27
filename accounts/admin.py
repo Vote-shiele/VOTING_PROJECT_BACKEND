d4f236f3c3a06dc44494ec9b018a37da63d9f652
Min Thu Khaing <151455290+MoriartyLink@users.noreply.github.com>
2025-06-27 14:01:14 +0630

    Add files via upload

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Admin, Poll, Candidate,Voter
+from .models import Admin, Poll, Candidate, Voter, AdminLog
+from .models import VoteLog
+
 
 admin.site.register(Admin)
 admin.site.register(Poll)
 admin.site.register(Candidate)
 admin.site.register(Voter)
+admin.site.register(VoteLog)
+admin.site.register(AdminLog)
 
 

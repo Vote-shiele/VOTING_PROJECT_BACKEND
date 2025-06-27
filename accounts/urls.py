b0f8cf9f2469672704c52a57c81f2eb45f2acdd1
Min Thu Khaing <151455290+MoriartyLink@users.noreply.github.com>
2025-06-27 14:00:29 +0630

    Add files via upload

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     path('add-candidate/<int:poll_id>/', views.add_candidate, name='add_candidate'),
     path('add-voter/<int:poll_id>/', views.add_voter, name='add_voter'),
     path('search/', views.search_polls, name='search_polls'),
+    path('poll-details/<int:poll_id>/', views.poll_details, name='poll_details'),
+    path('edit-poll/<int:poll_id>/', views.edit_poll, name='edit_poll'),
+    path('vote-log/<int:poll_id>/', views.vote_log, name='vote_log'),
+    path('edit-candidate/<int:candidate_id>/', views.edit_candidate, name='edit_candidate'),
+    path('delete-poll/<int:poll_id>/', views.delete_poll, name='delete_poll'),
 ]

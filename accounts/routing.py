e1f75ed8d9518f59b7c7eb3539094cfce78c0141
Min Thu Khaing <151455290+MoriartyLink@users.noreply.github.com>
2025-07-27 14:16:29 +0630

    Add files via upload

--- accounts/routing.py
+++ accounts/routing.py
@@ -1,6 +1,10 @@
 from django.urls import re_path
 from . import consumers
 
+# WebSocket URL routing patterns
 websocket_urlpatterns = [
+    # This route matches WebSocket connections at ws/votes/<poll_id>/
+    # <poll_id> is a dynamic part captured as a string (using \w+ = alphanumeric)
+    # The connection is handled by VoteConsumer (ASGI consumer)
     re_path(r'ws/votes/(?P<poll_id>\w+)/$', consumers.VoteConsumer.as_asgi()),
 ]
